724d50de5f068c20c9106a8a154a28211daf0274
Hany <hanynathan@yahoo.com>
2019-01-05 12:58:05 +0200

    add js cheat sheet

---
+++ JavaScriptCheatSheet.js
@@ -0,0 +1,9 @@
+//to return new copy (instance) of an object use function constructor
+const f = () => {
+  //return object
+  return {
+
+  }
+}
+
+const obj = new f(); //obj is a new copy of f object
